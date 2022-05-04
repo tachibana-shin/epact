dd408cfee3c35867cbb3490d36d1147b72a7dec7
tachibana-shin <45375496+tachibana-shin@users.noreply.github.com>
2022-05-04 06:24:37 +0000

    update for tsup

--- demo/express.config.ts
+++ demo/express.config.ts
@@ -1,6 +1,11 @@
+import path from "path";
+
 import { defineConfig } from "../src";
 
 export default defineConfig({
   port: 3000,
   boot: ["dotenv", "plugins"],
+  alias: {
+    src: path.join(__dirname, "src"),
+  },
 });
