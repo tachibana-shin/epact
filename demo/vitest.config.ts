0bd873bf03cc9ef82469deaadc1332ac335cf987
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-05-27 04:11:57 +0000

    update demo for vitest

--- demo/vitest.config.ts
+++ demo/vitest.config.ts
@@ -1,6 +1,7 @@
-import vitestTsconfigPaths from "vitest-tsconfig-paths"
+import epactPluginVitest from "epact/plugin-vitest";
 import { defineConfig } from "vitest/config";
 
+
 export default defineConfig({
-  plugins: [vitestTsconfigPaths()]
-})
+  plugins: [epactPluginVitest()],
+});
