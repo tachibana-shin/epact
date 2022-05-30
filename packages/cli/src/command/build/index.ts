3ef75335b869d92003fca89149730cdcc725ad15
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-05-30 14:13:10 +0000

    fix clear

--- packages/cli/src/command/build/index.ts
+++ packages/cli/src/command/build/index.ts
@@ -40,53 +40,50 @@ export default async function (
   const banner = toAppend(config.build?.banner)
   const footer = toAppend(config.build?.footer)
 
-  await Promise.all([
-    build({
-      entry: [join(pathToDir, `.express/${config.filename ?? "main.ts"}`)],
-      splitting: true,
-      clean: true,
-      // ...config.build,
-      watch: config.build?.watch,
-      outDir: config.build?.outDir,
-      keepNames: config.build?.keepNames,
-      target: config.build?.target,
-      ignoreWatch: config.build?.ignoreWatch,
-      onSuccess: config.build?.onSuccess,
-      inject: config.build?.inject,
-      external: config.build?.external,
-      jsxFactory: config.build?.jsxFactory,
-      jsxFragment: config.build?.jsxFragment,
-      silent: config.build?.silent,
-      metafile: config.build?.metafile,
-      ...options,
-      loader: config.loader,
-      minify: !(
-        options["no-minify"] ||
-        config.build?.noMinify ||
-        options.debug
-      ),
-      sourcemap: options.debug ? "inline" : options.sourcemap,
-      define: {
-        ...config.env,
-        ...options.env
-      },
-      env: {
-        ...config.env,
-        ...options.env
-      },
-      dts: false,
-      format: config.build?.format
-        ? Array.isArray(config.build?.format)
-          ? config.build.format
-          : [config.build.format]
-        : ["cjs"],
-      esbuildOptions(options, context) {
-        if (banner) options.banner = banner
-        if (footer) options.footer = footer
+  await build({
+    entry: [join(pathToDir, `.express/${config.filename ?? "main.ts"}`)],
+    splitting: true,
+    clean: true,
+    // ...config.build,
+    watch: config.build?.watch,
+    outDir: config.build?.outDir,
+    keepNames: config.build?.keepNames,
+    target: config.build?.target,
+    ignoreWatch: config.build?.ignoreWatch,
+    onSuccess: config.build?.onSuccess,
+    inject: config.build?.inject,
+    external: config.build?.external,
+    jsxFactory: config.build?.jsxFactory,
+    jsxFragment: config.build?.jsxFragment,
+    silent: config.build?.silent,
+    metafile: config.build?.metafile,
+    ...options,
+    loader: config.loader,
+    minify: !(options["no-minify"] || config.build?.noMinify || options.debug),
+    sourcemap: options.debug ? "inline" : options.sourcemap,
+    define: {
+      ...config.env,
+      ...options.env
+    },
+    env: {
+      ...config.env,
+      ...options.env
+    },
+    dts: false,
+    format: config.build?.format
+      ? Array.isArray(config.build?.format)
+        ? config.build.format
+        : [config.build.format]
+      : ["cjs"],
+    esbuildOptions(options, context) {
+      if (banner) options.banner = banner
+      if (footer) options.footer = footer
 
-        config.build?.esbuildOptions?.(options, context)
-      }
-    }),
+      config.build?.esbuildOptions?.(options, context)
+    }
+  })
+
+  await Promise.all([
     config.build?.pkgFile ? buildFilePkgJSON(pathToDir) : void 0,
     copyFilesInPublish(pathToDir)
   ])
