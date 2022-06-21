a9fefce92d55f3fac3c9d805c927d852fcf10c5b
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-06-21 08:32:10 +0000

    update demo

--- demo/src/pages/index.ts
+++ demo/src/pages/index.ts
@@ -1,26 +1,9 @@
-import http from "boot/plugins";
-import { router } from "epact";
-import { verify } from "jsonwebtoken";
+import { router } from "epact"
 
-export default router<{
-  // eslint-disable-next-line functional/prefer-readonly-type
-  get: {
-    // eslint-disable-next-line functional/prefer-readonly-type
-    params: {
-      // eslint-disable-next-line functional/prefer-readonly-type
-      id: number;
-    };
-    // eslint-disable-next-line functional/prefer-readonly-type
-    body: {
-      // eslint-disable-next-line functional/prefer-readonly-type
-      message: string;
-    };
-  };
-}>({
+export default router({
   get(req, res) {
     res.json({
-      message: "Hello express!",
-      user: req.user
-    });
+      message: "Hello express!"
+    })
   }
 })
