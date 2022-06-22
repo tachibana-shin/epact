b3be92f2f5f60d80c05693db794d18ec5a3fde07
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-06-22 02:36:20 +0000

    update createPage

--- packages/epact/src/createPage.ts
+++ packages/epact/src/createPage.ts
@@ -1,3 +1,6 @@
+import { basename } from "path"
+import { isTypedArray } from "util/types"
+
 import type {
   ErrorRequestHandler,
   NextFunction,
@@ -6,7 +9,7 @@ import type {
   Response
 } from "express"
 import { Router } from "express"
-import type { ParamsDictionary } from "express-serve-static-core"
+import type { CookieOptions, ParamsDictionary } from "express-serve-static-core"
 import type { ParsedQs } from "qs"
 
 import alwayIsArray from "./utils/alwayIsArray"
@@ -40,33 +43,206 @@ type Methods =
   | "unlock"
   | "unsubscribe"
 
-export interface RequestHandlerCustom<
-  P = ParamsDictionary,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  ResBody = any,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  ReqBody = any,
-  ReqQuery = ParsedQs,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  Locals extends Record<string, any> = Record<string, any>
-> {
-  (
-    req: Request<P, ResBody, ReqBody, ReqQuery, Locals>,
-    res: Response<ResBody, Locals>
-  ): void
-  (
-    req: Request<P, ResBody, ReqBody, ReqQuery, Locals>,
-    res: Response<ResBody, Locals>,
-    next: NextFunction
-  ): void
+// eslint-disable-next-line functional/no-mixed-type
+interface SendFileOptions {
+  filepath: string
+  cwd?: string
+  maxAge?: number
+  lastModified?: number
+  headers?: Record<string, string>
+  dotFiles?: "allow" | "deny" | "ignore"
+  acceptRanges?: boolean
+  cacheControl?: boolean
+  immutable?: boolean
+  callback?: (err?: Error) => void
 }
+interface CookieValueOption extends CookieOptions {
+  value: string | object
+}
+
+interface NormalResponseDataOption<ResBody> {
+  data?: ResBody
+  rawData?: ResBody
+  jsonp?: ResBody
+  file?: string | SendFileOptions
+  download?: string | SendFileOptions
+}
+interface RedirectResponseDataOption {
+  redirect?:
+    | string
+    | "back"
+    | {
+        status?: number
+        path: string | "back"
+      }
+}
+interface RenderResponseDataOption {
+  render?:
+    | string
+    | {
+        view: string
+        locals?: Record<string, unknown>
+        callback?: ((err: Error) => void) | ((err: null, html: string) => void)
+      }
+}
+type ResponseData<
+  ResBody,
+  StatusCode extends number,
+  Locals = Record<string, unknown>
+> =
+  | ({
+      status?: number
+      sendStatus?: StatusCode
+      links?: Record<string, string>
+
+      type?: string
+      format?: Record<string, () => void>
+      attachment?: string | true
+      headers?: Record<string, string | string[]>
+      appendHeaders?: Record<string, string | string[]>
+      cookie?: Record<string, string | CookieValueOption>
+      location?: string | "back"
+      end?: true | ResBody
+
+      locals?: Locals
+    } & (NormalResponseDataOption<ResBody> &
+      RedirectResponseDataOption &
+      RenderResponseDataOption))
+  | (ResBody & {
+      message: string
+    })
+function isNotNull<T>(value: T): value is Exclude<T, void> {
+  return value !== null && value !== undefined
+}
+const propsStatic = [
+  "status",
+  "sendStatus",
+  "links",
+  "type",
+  "format",
+  { type: "attachment", true: true },
+  { type: "location", true: true },
+  "end"
+]
+function runResponseByData(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data: ResponseData<any, number, any>,
+  res: Response
+) {
+  if ("message" in data) {
+    res.json(data)
+
+    return
+  }
+  // eslint-disable-next-line functional/no-let
+  for (let i = 0; i < propsStatic.length; i++) {
+    const v = propsStatic[i]
+    // eslint-disable-next-line functional/no-let
+    let noTrue = false
+    const propName = (typeof v === "object" ? ((noTrue = true), v.type) : v) as
+      | "status"
+      | "sendStatus"
+      | "links"
+      | "type"
+      | "format"
+      | "attachment"
+      | "location"
+      | "end"
+
+    if (propName in data) {
+      // accept
+      const value =
+        noTrue && data[propName] === true ? undefined : data[propName]
+
+      res[propName](value)
+    }
+  }
+  if (isNotNull(data.headers)) res.set(data.headers)
+  if (isNotNull(data.appendHeaders)) {
+    for (const key in data.appendHeaders)
+      res.append(key, data.appendHeaders[key])
+  }
+  if (isNotNull(data.cookie)) {
+    for (const name in data.cookie) {
+      const val = data.cookie[name]
+
+      if (typeof val === "string") res.cookie(name, val)
+      else res.cookie(name, val.value, val)
+    }
+  }
+  if (isNotNull(data.locals)) Object.assign(res.locals, data.locals)
+
+  if (isNotNull(data.data)) {
+    if (typeof data.data === "object") {
+      if (isTypedArray(data.data)) res.send(Buffer.from(data.data))
+      else res.json(data.data)
+    } else {
+      res.send(data.data)
+    }
+  } else if (isNotNull(data.rawData)) {
+    res.send(data.rawData)
+  } else if (isNotNull(data.jsonp)) {
+    res.jsonp(data.jsonp)
+  } else if (isNotNull(data.file)) {
+    const { file } = data
+    if (typeof file === "string") res.sendFile(file)
+    else res.sendFile(file.filepath, file, file.callback)
+  } else if (isNotNull(data.download)) {
+    const { download } = data
+    if (typeof download === "string") {
+      res.download(download)
+    } else {
+      res.download(
+        download.filepath,
+        basename(download.filepath),
+        download,
+        download.callback
+      )
+    }
+  }
+}
+
+type OrPromise<T> = T | Promise<T>
+
+type FixArrayQs<Qs = ParsedQs> = {
+  [name in keyof Qs]: Qs[name] extends string[] ? string | string[] : Qs[name]
+}
+
+export type RequestHandlerCustom<
+  P = ParamsDictionary,
+  ResBody = unknown,
+  ReqBody = unknown,
+  ReqQuery = ParsedQs,
+  Locals = Record<string, unknown>
+> = (
+  this: Request<
+    Partial<P>,
+    ResBody,
+    ReqBody extends object ? Partial<ReqBody> : ReqBody,
+    Partial<FixArrayQs<ReqQuery>>,
+    Locals
+  > & {
+    res: Response<ResBody, Locals>
+    return: <D extends ResponseData<ResBody, number, Locals> | void>(
+      data: D
+    ) => D
+  },
+  req: Request<
+    Partial<P>,
+    ResBody,
+    ReqBody extends object ? Partial<ReqBody> : ReqBody,
+    Partial<ReqQuery>,
+    Locals
+  >,
+  res: Response<ResBody, Locals>,
+  next: NextFunction
+) => OrPromise<void | ResponseData<ResBody, number, Locals>>
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function loadMiddleware(list?: any) {
   if (!list) return null
 
   if (
-    typeof list === "string" ||
     typeof list === "function" ||
     Array.isArray(list)
   ) {
@@ -96,7 +272,8 @@ export default function createPage(
   prefix: string,
   $page: ReturnType<typeof page>
 ) {
-  const middleware = loadMiddleware($page.middleware)
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const middleware = loadMiddleware(($page as any).middleware)
 
   if ($page instanceof Router) {
     // cracker;
@@ -166,49 +343,71 @@ export default function createPage(
     router
   }
 }
+const noop = (v: unknown) => v
 
 function createWrapRequestHandler(
   callback: RequestHandlerCustom
 ): RequestHandler {
-  switch (callback.length) {
-    case 2:
-      return (req, res, next) => {
-        try {
-          callback(req, res)
-        } catch (err) {
-          next(err)
-        }
-      }
-    case 3:
-      return (req, res, next) => {
-        try {
-          callback(req, res, next)
-        } catch (err) {
-          next(err)
-        }
-      }
+  if (callback.length <= 3) {
+    return async (req, res, next) => {
+      // eslint-disable-next-line functional/immutable-data, @typescript-eslint/no-explicit-any
+      if (!(req as any).return) (req as unknown as any).return = noop
 
-    default:
-      return callback
+      try {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const data = await callback.call(req as any, req, res, next)
+        if (data) runResponseByData(data, res)
+      } catch (err) {
+        next(err)
+      }
+    }
   }
+
+  return callback
 }
 
 type TypeOrArray<T> = T | T[]
 
-function page(
+interface OptsMethod {
+  body?: unknown
+  data?: unknown
+  locals?: Record<string, unknown>
+}
+function page<
+  Opts extends {
+    params?: Record<string, string> | string
+    query?: ParsedQs
+  } & {
+    // eslint-disable-next-line no-unused-vars
+    [name in Methods]?: OptsMethod
+  }
+>(
   opts:
     | ({
         middleware?:
           | TypeOrArray<RequestHandler>
           | {
               // eslint-disable-next-line no-unused-vars
-              [name in Methods]?: TypeOrArray<
+              [name in Exclude<keyof Opts, "params" | "query">]?: TypeOrArray<
                 ErrorRequestHandler | RequestHandler
               >
             }
       } & {
-        // eslint-disable-next-line no-unused-vars
-        [name in Methods]?: TypeOrArray<RequestHandlerCustom>
+        [name in Exclude<keyof Opts, "params" | "query">]: TypeOrArray<
+          RequestHandlerCustom<
+            Opts["params"] extends infer R extends string
+              ? Record<R, string>
+              : Opts["params"],
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            Opts[name]["data"],
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            Opts[name]["body"],
+            Opts["query"],
+            Record<string, unknown>
+          >
+        >
       })
     | RequestHandlerCustom
 ) {
